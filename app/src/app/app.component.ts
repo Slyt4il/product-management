1c80e8e7088135b7fd755cda7fea96ccd94d2ffa
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2026-02-02 00:44:17 +0700

    Create Product List Component (Challenge 1)

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>Hello World!</h1>`,
-  styles: []
+  imports: [ProductListComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
---
+++ app/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/products';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  exportAs: 'productList',
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Fried Rice',
+      sku: 'DISH001',
+      price: 45,
+      stock: 20,
+      category: 'Food',
+      createdAt: new Date().toISOString()
+    },
+    {
+      id: 2,
+      name: 'Orange Juice',
+      sku: 'DRINK001',
+      price: 25,
+      stock: 50,
+      category: 'Beverages',
+      createdAt: new Date().toISOString()
+    },
+    {
+      id: 3,
+      name: 'Soap',
+      sku: 'ITEM001',
+      price: 35,
+      stock: 0,
+      category: 'PersonalItems',
+      createdAt: new Date().toISOString()
+    },
+    {
+      id: 4,
+      name: 'T-Shirt',
+      sku: 'CLOTH001',
+      price: 299,
+      stock: 5,
+      category: 'Clothing',
+      createdAt: new Date().toISOString()
+    },
+    {
+      id: 5,
+      name: 'Banana',
+      sku: 'FRUIT001',
+      price: 1,
+      stock: 0,
+      category: 'Food',
+      createdAt: new Date().toISOString()
+    }
+  ];
+}
